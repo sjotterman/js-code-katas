e0a0c03cab0c79f67cc0e9395c3333ccdf095f48
Samuel Otterman <samuelotterman@futurimedia.com>
2018-09-28 16:27:05 -0400

    add describe to tests

--- src/fizzbuzz.test.js
+++ src/fizzbuzz.test.js
@@ -1,19 +1,23 @@
 const fizzbuzz = require('./fizzbuzz');
 
-test('Calculates first value', () => {
-  expect(fizzbuzz(1)).toEqual([1]);
-});
+describe ('Fizzbuzz', function (){
 
-test('Calculates first two values', () => {
-  expect(fizzbuzz(2)).toEqual([1, 2]);
-});
+  it('Calculates first value', () => {
+    expect(fizzbuzz(1)).toEqual([1]);
+  });
 
-test('Handles first instance of divisible by 3', () => {
-  expect(fizzbuzz(3)).toEqual([1, 2, 'Fizz']);
-});
+  it('Calculates first two values', () => {
+    expect(fizzbuzz(2)).toEqual([1, 2]);
+  });
 
-test('Handles 1 through 20', () => {
-  let resultArray = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz',
-'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz'];
-  expect(fizzbuzz(20)).toEqual(resultArray);
-});
+  it('Handles first instance of divisible by 3', () => {
+    expect(fizzbuzz(3)).toEqual([1, 2, 'Fizz']);
+  });
+
+  it('Handles 1 through 20', () => {
+    let resultArray = [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz',
+  'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz', 16, 17, 'Fizz', 19, 'Buzz'];
+    expect(fizzbuzz(20)).toEqual(resultArray);
+  });
+
+});;
