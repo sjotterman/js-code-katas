d791cdc416e98996b342f41883436b041b504def
Samuel Otterman <sjotterman@gmail.com>
2019-08-11 13:04:35 -0400

    read files from CSV

---
+++ src/reader.js
@@ -0,0 +1,5 @@
+// Create an array of arrays by reading in lines
+// from a CSV at filePath
+const createArray = filePath => {};
+
+module.exports = { createArray };
---
+++ src/reader.test.js
@@ -0,0 +1,21 @@
+const reader = require("./reader");
+
+describe("CSV reader", function() {
+  it("reads first line from a csv", () => {
+    const oneLineArray = ["John", "Doe", "34", "M", "Akron", "OH"];
+    expect(reader.createArray("./src/files/oneLine.csv")).toEqual([
+      oneLineArray
+    ]);
+  });
+
+  it("reads multiple lines from a csv", () => {
+    const multiLineArray = [
+      ["John", "Doe", "34", "M", "Akron", "OH"],
+      ["Jane", "Smith", "32", "F", "Detroit", "MI"],
+      ["Mike", "White", "28", "M", "Sacramento", "CA"]
+    ];
+    expect(reader.createArray("src/files/multiLines.csv")).toEqual(
+      multiLineArray
+    );
+  });
+});
